8cdd36b5fa171766998e2694801c6ca1f66632c0
mohanson <mohanson@outlook.com>
2021-10-29 11:40:19 +0800

    Update vvaddint256 example

--- res/vvaddint256.c
+++ res/vvaddint256.c
@@ -4,16 +4,21 @@
 void vvaddint256(size_t n, const uint64_t x[][4], const uint64_t y[][4], uint64_t z[][4]);
 
 int main() {
-  uint64_t x[1][4] = {{0x0000000000000001, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000}};
-  uint64_t y[1][4] = {{0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff}};
+  uint64_t x[1][4] = {{0x0000000000000001, 0x0000000000000002, 0x0000000000000003, 0x0000000000000004}};
+  uint64_t y[1][4] = {{0xfffffffffffffffe, 0xfffffffffffffffd, 0xfffffffffffffffc, 0xfffffffffffffffb}};
   uint64_t z[1][4];
   vvaddint256(1, x, y, z);
 
-  uint64_t s = 0;
-  for (int i = 0; i < 4; i++) {
-    s += z[0][i];
+  if (z[0][0] != 0xffffffffffffffff) {
+    return 1;
   }
-  if (s != 0) {
+  if (z[0][1] != 0xffffffffffffffff) {
+    return 1;
+  }
+  if (z[0][2] != 0xffffffffffffffff) {
+    return 1;
+  }
+  if (z[0][3] != 0xffffffffffffffff) {
     return 1;
   }
   return 0;
