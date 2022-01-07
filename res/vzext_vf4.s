16ccffefe7daef563531f58bca0ee43277ae28f8
mohanson <mohanson@outlook.com>
2022-01-07 16:59:27 +0800

    2022-01-07 16:59:26

--- res/vzext_vf4.s
+++ res/vzext_vf4.s
@@ -3,12 +3,13 @@
     .global vzext_vf4
 vzext_vf4:
     vsetvli t0, a0, e256, m1, ta, ma
-    vle256.v v0, (a1)
+    vle64.v v0, (a1)
       sub a0, a0, t0
-      slli t0, t0, 5
+      slli t0, t0, 3
       add a1, a1, t0
     vzext.vf4 v1, v0
     vse256.v v1, (a2)
+      slli t0, t0, 2
       add a2, a2, t0
       bnez a0, vzext_vf4
       ret
