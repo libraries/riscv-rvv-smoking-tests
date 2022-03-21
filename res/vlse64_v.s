dffa6ed9b150191999b05ef55ab56df5a5cd9c0d
mohanson <mohanson@outlook.com>
2022-03-21 17:06:29 +0800

    Load/store stride could be negative

--- res/vlse64_v.s
+++ res/vlse64_v.s
@@ -2,14 +2,13 @@
     .balign 4
     .global vlse64_v
 vlse64_v:
-    li t3, 32
     vsetvli t0, a0, e64, m1, ta, ma
-    vlse64.v v1, (a1), t3
+    vlse64.v v1, (a1), a3
       sub a0, a0, t0
-      slli t0, t0, 5
-      add a1, a1, t0
+      mul t1, t0, a3
+      add a1, a1, t1
     vse64.v v1, (a2)
-      srli t0, t0, 2
-      add a2, a2, t0
+      slli t1, t0, 3
+      add a2, a2, t1
       bnez a0, vlse64_v
       ret
--- res/vsse64_v.s
+++ res/vsse64_v.s
@@ -2,14 +2,13 @@
     .balign 4
     .global vsse64_v
 vsse64_v:
-    li t3, 32
     vsetvli t0, a0, e64, m1, ta, ma
     vle64.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 3
-      add a1, a1, t0
-    vsse64.v v1, (a2), t3
-      slli t0, t0, 2
-      add a2, a2, t0
+      slli t1, t0, 3
+      add a1, a1, t1
+    vsse64.v v1, (a2), a3
+      mul t1, t0, a3
+      add a2, a2, t1
       bnez a0, vsse64_v
       ret
