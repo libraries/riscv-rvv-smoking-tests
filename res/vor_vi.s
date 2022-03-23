64d0f6e54e927f3943fc4d5ac772e42f8c7e4d44
mohanson <mohanson@outlook.com>
2022-03-23 17:23:42 +0800

    Rewrite bitwise logical

--- res/vor_vi.s
+++ res/vor_vi.s
@@ -2,13 +2,13 @@
     .balign 4
     .global vor_vi
 vor_vi:
-    vsetvli t0, a0, e256, m1, ta, ma
-    vle256.v v0, (a1)
+    vsetvli t0, a0, e64, m1, ta, ma
+    vle64.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 5
+      slli t0, t0, 3
       add a1, a1, t0
-    vor.vi v1, v0, 1
-    vse256.v v1, (a2)
+    vor.vi v2, v1, 1
+    vse64.v v2, (a2)
       add a2, a2, t0
       bnez a0, vor_vi
       ret
