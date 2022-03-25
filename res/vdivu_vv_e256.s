25f9fe386c50b344bbc68c00429b174c37fdf8a7
mohanson <mohanson@outlook.com>
2022-03-25 14:47:30 +0800

    Rewrite divide

--- res/vdivu_vv_e256.s
+++ res/vdivu_vv_e256.s
@@ -2,15 +2,15 @@
     .balign 4
     .global vdivu_vv
 vdivu_vv:
-    vsetvli t0, a0, e64, m1, ta, ma
-    vle64.v v1, (a1)
+    vsetvli t0, a0, e256, m1, ta, ma
+    vle256.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 3
+      slli t0, t0, 5
       add a1, a1, t0
-    vle64.v v2, (a2)
+    vle256.v v2, (a2)
       add a2, a2, t0
     vdivu.vv v3, v1, v2
-    vse64.v v3, (a3)
+    vse256.v v3, (a3)
       add a3, a3, t0
       bnez a0, vdivu_vv
       ret
