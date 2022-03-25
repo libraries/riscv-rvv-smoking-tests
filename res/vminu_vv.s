e7f38ef9f008f864a8bf41190f3b441ca2b4a243
mohanson <mohanson@outlook.com>
2022-03-25 13:38:34 +0800

    Rewrite min/max

--- res/vminu_vv.s
+++ res/vminu_vv.s
@@ -2,15 +2,15 @@
     .balign 4
     .global vminu_vv
 vminu_vv:
-    vsetvli t0, a0, e256, m1, ta, ma
-    vle256.v v0, (a1)
+    vsetvli t0, a0, e64, m1, ta, ma
+    vle64.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 5
+      slli t0, t0, 3
       add a1, a1, t0
-    vle256.v v1, (a2)
+    vle64.v v2, (a2)
       add a2, a2, t0
-    vminu.vv v2, v0, v1
-    vse256.v v2, (a3)
+    vminu.vv v3, v1, v2
+    vse64.v v3, (a3)
       add a3, a3, t0
       bnez a0, vminu_vv
       ret
--- res/vminu_vx.s
+++ res/vminu_vx.s
@@ -2,13 +2,13 @@
     .balign 4
     .global vminu_vx
 vminu_vx:
-    vsetvli t0, a0, e256, m1, ta, ma
-    vle256.v v0, (a1)
+    vsetvli t0, a0, e64, m1, ta, ma
+    vle64.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 5
+      slli t0, t0, 3
       add a1, a1, t0
-    vminu.vx v1, v0, a2
-    vse256.v v1, (a3)
+    vminu.vx v3, v1, a2
+    vse64.v v3, (a3)
       add a3, a3, t0
       bnez a0, vminu_vx
       ret
