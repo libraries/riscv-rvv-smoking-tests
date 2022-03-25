b3905f9606471969e6fda8afe4150afbb5c33ec6
mohanson <mohanson@outlook.com>
2022-03-25 14:05:30 +0800

    Rewrite single width multiply

--- res/vmulh_vx_e256.s
+++ res/vmulh_vx_e256.s
@@ -2,13 +2,13 @@
     .balign 4
     .global vmulh_vx
 vmulh_vx:
-    vsetvli t0, a0, e64, m1, ta, ma
-    vle64.v v1, (a1)
+    vsetvli t0, a0, e256, m1, ta, ma
+    vle256.v v1, (a1)
       sub a0, a0, t0
-      slli t0, t0, 3
+      slli t0, t0, 5
       add a1, a1, t0
     vmulh.vx v2, v1, a2
-    vse64.v v2, (a3)
+    vse256.v v2, (a3)
       add a3, a3, t0
       bnez a0, vmulh_vx
       ret
