dcfb7390140c063ece30eebc7b052f2117397f2d
mohanson <mohanson@outlook.com>
2022-03-28 20:46:42 +0800

    Add vsmul

---
+++ res/vsmul_vv.s
@@ -0,0 +1,16 @@
+    .text
+    .balign 4
+    .global vsmul_vv
+vsmul_vv:
+    vsetvli t0, a0, e64, m1, ta, ma
+    vle64.v v0, (a1)
+      sub a0, a0, t0
+      slli t0, t0, 3
+      add a1, a1, t0
+    vle64.v v1, (a2)
+      add a2, a2, t0
+    vsmul.vv v2, v0, v1
+    vse64.v v2, (a3)
+      add a3, a3, t0
+      bnez a0, vsmul_vv
+      ret
