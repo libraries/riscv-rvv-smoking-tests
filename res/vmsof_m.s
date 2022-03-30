4dc602bfa9d3a6dc5a9c515ca0bd5101d729cb04
mohanson <mohanson@outlook.com>
2022-03-30 15:32:04 +0800

    Add sbf/sif/sof

---
+++ res/vmsof_m.s
@@ -0,0 +1,13 @@
+    .text
+    .balign 4
+    .global vmsof_m
+vmsof_m:
+    vsetvli t0, a0, e32, m1, ta, ma
+    vmv.v.x v2, a1
+    vmv.v.x v0, a2
+    vsetvli t0, a0, e8, m2, ta, ma
+    vmxor.mm v4, v4, v4
+    vmsof.m v4, v2, v0.t
+    vsetvli t0, a0, e32, m1, ta, ma
+    vmv.x.s a0, v4
+    ret
